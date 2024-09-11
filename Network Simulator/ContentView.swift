83aa62137869fcd984ad3724676436901d2e3e87
TsangHingTat <125679650+TsangHingTat@users.noreply.github.com>
2024-09-11 19:51:01 +0800

    Initial Commit

---
+++ Network Simulator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Network Simulator
+//
+//  Created by HingTatTsang on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Network Simulator/Network_SimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Network_SimulatorApp.swift
+//  Network Simulator
+//
+//  Created by HingTatTsang on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Network_SimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
