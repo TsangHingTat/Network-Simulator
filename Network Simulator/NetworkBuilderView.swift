6500e85b6b3316fff209bfd52a2fb0aa111fac8f
TsangHingTat <125679650+TsangHingTat@users.noreply.github.com>
2024-09-15 16:59:53 +0800

    Update

---
+++ Network Simulator/NetworkBuilderView.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkBuilderView.swift
+//  Network Simulator
+//
+//  Created by HingTatTsang on 9/15/24.
+//
+
+import SwiftUI
+
+struct NetworkBuilderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NetworkBuilderView()
+}
